5ccbacaeff3093db54cfc9f3f5e833bdeacb840d
yunlongn <yunlongn@outlook.com>
2024-11-18 20:00:50 +0800

    1

--- main.py
+++ main.py
@@ -139,6 +139,8 @@ def updateChannelUrlsM3U(channels, template_channels):
                                     written_urls.add(url)
 
                             total_urls = len(filtered_urls)
+                            if total_urls >= 1:
+                                f_m3u.write(f"#EXTINF:-1 tvg-id=\"{index}\" tvg-name=\"{channel_name}(IPV6)\" tvg-logo=\"https://live.fanmingming.com/tv/{channel_name}.png\" group-title=\"{category}(IPV6)\",{channel_name}\n")
                             for index, url in enumerate(filtered_urls, start=1):
                                 if is_ipv6(url):
                                     url_suffix = f"$LR•IPV6" if total_urls == 1 else f"$LR•IPV6『线路{index}』"
@@ -151,13 +153,11 @@ def updateChannelUrlsM3U(channels, template_channels):
 
                                 new_url = f"{base_url}{url_suffix}"
                                 if is_ipv6(url):
-                                    f_m3u.write(f"#EXTINF:-1 tvg-id=\"{index}\" tvg-name=\"{channel_name}(IPV6)\" tvg-logo=\"https://live.fanmingming.com/tv/{channel_name}.png\" group-title=\"{category}(IPV6)\",{channel_name}\n")
-                                    f_m3u.write(new_url + "\n")
                                     f_txt.write(f"{channel_name}(IPV6),{new_url}\n")
                                 else:
-                                    f_m3u.write(f"#EXTINF:-1 tvg-id=\"{index}\" tvg-name=\"{channel_name}\" tvg-logo=\"https://live.fanmingming.com/tv/{channel_name}.png\" group-title=\"{category}\",{channel_name}\n")
-                                    f_m3u.write(new_url + "\n")
                                     f_txt.write(f"{channel_name},{new_url}\n")
+
+                                f_m3u.write(new_url + "\n")
 
 
             f_txt.write("\n")
