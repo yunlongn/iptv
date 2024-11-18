a080800d4d7d56141f4df3df691f5e740a96d82c
yunlongn <yunlongn@outlook.com>
2024-11-18 20:14:00 +0800

    1

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 ip_version_priority = "ipv6"
 
 source_urls = [
+    "https://aktv.top/live.m3u",
     "http://175.178.251.183:6689/aktvlive.txt",
     "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
     "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
